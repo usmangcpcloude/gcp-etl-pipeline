f2b26421f9aef77ed4b7ef16e4186cd3260b4259
Admin <admin@addo.ai>
2025-02-27 01:07:07 +0500

    update

--- composer/goSales.py
+++ composer/goSales.py
@@ -15,8 +15,7 @@ default_args = {
     'depends_on_past': False,
     'email_on_failure': True,
     'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retries': 0,
     'start_date': datetime(2024, 2, 23),
 }
 
